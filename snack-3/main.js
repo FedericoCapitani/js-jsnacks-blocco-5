38854527ba08a34610d15bf368355a35f09e09e1
FedericoCapitani <federico.capitani@outlook.it>
2022-04-12 12:01:14 +0200

    snack 3

--- snack-3/main.js
+++ snack-3/main.js
@@ -1,3 +1,19 @@
 /* SNACK 3 
 Scrivi una funzione che accetti una stringa come argomento e la ritorni girata (es. Ciao -> oaiC)
  */
+function reverseString(str){
+    // divido la stringa in singoli caratteri
+    let split_string = str.split("");
+    // inverto i singoli caratteri e li metto dentro un array
+    let reverse_array = split_string.reverse()
+    // riunisco i caratteri in una stringa
+    let joined_array = reverse_array.join("")
+
+    return joined_array
+}
+
+const string = 'Ciao, come stai?'
+
+let reverse_string = reverseString(string)
+
+console.log(reverse_string);
